81e34f80625c84cc7f4fe1055f1471aec0e98cb0
birhasibe <101066816+birhasibe@users.noreply.github.com>
2022-09-28 18:33:06 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+import { Route } from "react-router-dom";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import { connect } from "react-redux";
+
+const App = (props) => {
+  console.log(props.bookList);
+  return (
+    <div className="App">
+      <h1>
+        Alışveriş Sepeti Yapımı
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        React Dersleri
+      </h1>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    bookList: state.bookList,
+  };
+};
+export default connect(mapStateToProps)(App);
